e1ad7fee244803fb3a50210d96f16ac860514888
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-11-07 22:26:06 +0545

    Added Random function

--- static/script.js
+++ static/script.js
@@ -2,6 +2,7 @@
 let blackjackGame = {
     'you':{'scoreSpan': '#your-blackjack-result','div':'#your-box','score':0},
     'dealer':{'scoreSpan': '#dealer-blackjack-result','div':'#dealer-box','score':0},
+    'cards':['2','3','4','5','6','7','8','9','K','J','Q','A']
 };
 
 const YOU = blackjackGame['you']
@@ -10,16 +11,21 @@ const DEALER = blackjackGame['dealer']
 const hitSound = new Audio('static/sounds/swish.m4a');
 
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
-
 document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
 function blackjackHit(){
-    showCard(YOU);
+    let card = randomCard();
+    showCard(card,YOU);
 }
 
-function showCard(activePlayer){
+function randomCard(){
+    let randomIndex = Math.floor(Math.random()*13);
+    return blackjackGame['cards'][randomIndex];
+}
+
+function showCard(card,activePlayer){
     let cardImage = document.createElement('img');
-    cardImage.src = 'static/images/Q.png';
+    cardImage.src = `static/images/${card}.png`;
     document.querySelector(activePlayer['div']).appendChild(cardImage);
     hitSound.play();
 }
@@ -35,3 +41,4 @@ function blackjackDeal(){
         dealerImages[j].remove();
     };
 }
+
