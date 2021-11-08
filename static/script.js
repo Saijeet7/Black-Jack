e998a9dceba1b4b889097e7ca7cdee1004bd2670
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-11-08 09:57:47 +0545

    Added: Bust Logic

--- static/script.js
+++ static/script.js
@@ -2,7 +2,8 @@
 let blackjackGame = {
     'you':{'scoreSpan': '#your-blackjack-result','div':'#your-box','score':0},
     'dealer':{'scoreSpan': '#dealer-blackjack-result','div':'#dealer-box','score':0},
-    'cards':['2','3','4','5','6','7','8','9','K','J','Q','A']
+    'cards':['2','3','4','5','6','7','8','9','10','K','J','Q','A'],
+    'cardsMap':{'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'K':10,'J':10,'Q':10,'A':[1,11]},
 };
 
 const YOU = blackjackGame['you']
@@ -16,6 +17,8 @@ document.querySelector('#blackjack-deal-button').addEventListener('click', black
 function blackjackHit(){
     let card = randomCard();
     showCard(card,YOU);
+    updateScore(card,YOU);
+    showScore(YOU);
 }
 
 function randomCard(){
@@ -24,10 +27,12 @@ function randomCard(){
 }
 
 function showCard(card,activePlayer){
-    let cardImage = document.createElement('img');
-    cardImage.src = `static/images/${card}.png`;
-    document.querySelector(activePlayer['div']).appendChild(cardImage);
-    hitSound.play();
+    if(activePlayer['score']<=21){
+        let cardImage = document.createElement('img');
+        cardImage.src = `static/images/${card}.png`;
+        document.querySelector(activePlayer['div']).appendChild(cardImage);
+        hitSound.play();    
+    }
 }
 
 function blackjackDeal(){
@@ -42,3 +47,24 @@ function blackjackDeal(){
     };
 }
 
+function updateScore(card, activePlayer){
+    if(card=='A'){
+   // If adding 11 keeps me below 21, add 11. Otherwise, add 1
+        if (activePlayer['score']+blackjackGame['cardsMap'][card] <= 21){
+            activePlayer['score']+=blackjackGame['cardsMap'][card][1];
+        }else{
+            activePlayer['score']+=blackjackGame['cardsMap'][card][0];
+        }
+    }else{
+        activePlayer['score'] += blackjackGame['cardsMap'][card];
+    }
+}
+
+function showScore(activePlayer){
+    if (activePlayer['score']>21){
+        document.querySelector(activePlayer['scoreSpan']).textContent = 'BUST';
+        document.querySelector(activePlayer['scoreSpan']).style.color = 'red';
+    }else{  
+        document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score'];
+    }
+}
